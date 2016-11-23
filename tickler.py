864d739d2ca018674838c4036da28524806d2dd7
Simon Albinsson <zinob@aevy.com>
2016-11-23 14:27:10 +0100

    Improved logging using the logger library
    logger is better than commenting and un-commenting rows.

--- tickler.py
+++ tickler.py
@@ -1,53 +1,66 @@
 import sys
 import time
+import logging
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
 from cassandra.metadata import protect_name
 from cassandra import ConsistencyLevel
 
+logging.basicConfig(level=logging.WARN)
 # Get the arguments
-cass_keyspace = sys.argv[1]  # Cassandra Keyspace containing the Table to tickle
-cass_table = sys.argv[2]  # Cassandra Table to tickle
-cass_ip = sys.argv[3]  # Cassandra Port
-cass_port = sys.argv[4]  # CQL Port
-cass_throttle = sys.argv[5]  # microseconds
+def main():
+    cass_keyspace = sys.argv[1]  # Cassandra Keyspace containing the Table to tickle
+    cass_table = sys.argv[2]  # Cassandra Table to tickle
+    cass_ip = sys.argv[3]  # Cassandra Port
+    cass_port = sys.argv[4]  # CQL Port
+    cass_throttle = sys.argv[5]  # microseconds
 
-# print cass_keyspace  # debug
-# print cass_table  # debug
-# print cass_ip  # debug
-# print cass_port  # debug
-# print cass_throttle  # debug
+    logging.info("keyspace "+ cass_keyspace )
+    logging.info("table "+ cass_table )
+    logging.info("ip "+ cass_ip )
+    logging.info("port "+ cass_port )
+    logging.info("throttle "+ cass_throttle )
 
-# Set the connections to the cluster
-cluster = Cluster(
-    [cass_ip],
-    port=cass_port, compression=False)
+    # Set the connections to the cluster
+    cluster = Cluster(
+        [cass_ip],
+        port=cass_port, compression=False)
 
-# Connect to the KS
-session = cluster.connect(cass_keyspace)
+    # Connect to the KS
+    session = cluster.connect(cass_keyspace)
 
 
-# get the primary key of the table
-# May have interesting behavior if the table has a composite PK
-primary_key = cluster.metadata.keyspaces[cass_keyspace].tables[cass_table].primary_key[0].name
-# print primary_key # debug
+    # get the primary key of the table
+    # May have interesting behavior if the table has a composite PK
+    primary_key = cluster.metadata.keyspaces[cass_keyspace].tables[cass_table].primary_key[0].name
+    logging.info("primary key: "+  str(primary_key) )
 
-if primary_key:
-    # read every key of the table
-    query = 'SELECT {} FROM {}'.format(protect_name(primary_key),protect_name(cass_table)) 
-    statement = SimpleStatement(query, fetch_size=1000, consistency_level=ConsistencyLevel.ONE)
-    print 'Starting to repair table ' + cass_table
-    repair_query = 'SELECT COUNT(1) FROM {} WHERE {} = ?'.format(protect_name(cass_table),
-                                                                 protect_name(primary_key))
-    repair_statement = session.prepare(repair_query)
-    repair_statement.consistency_level = ConsistencyLevel.ALL
-    row_count = 0
-    for user_row in session.execute(statement):
-        # print user_row.id  # debug
-        row_count += 1
-        session.execute(repair_statement, [user_row[0]])
-        time.sleep(float(cass_throttle) / 1000000)  # delay in microseconds between reading each row
-        if (row_count % 1000) == 0:
-            print str(row_count) + ' rows processed'
-    print 'Repair of table ' + cass_table + ' complete'
-    print str(row_count) + ' rows read and repaired'
+    if primary_key:
+        # read every key of the table
+        query = 'SELECT {} FROM {}'.format(protect_name(primary_key),protect_name(cass_table)) 
+        statement = SimpleStatement(query, fetch_size=1000, consistency_level=ConsistencyLevel.QUORUM)
+        print 'Starting to repair table ' + cass_table
+        repair_query = 'SELECT COUNT(1) FROM {} WHERE {} = ?'.format(protect_name(cass_table),
+                                                                     protect_name(primary_key))
+        repair_statement = session.prepare(repair_query)
+        repair_statement.consistency_level = ConsistencyLevel.ALL
+        row_count = 0
+        last = time.time()
+        start_time = last
+        print_interval = 1000
+        idle_time = float(cass_throttle) / 1000000
+        for user_row in session.execute(statement):
+            logging.debug("reading row: " + repr(user_row) ) 
+            row_count += 1
+            session.execute(repair_statement, [user_row[0]])
+            time.sleep(idle_time)  # delay in microseconds between reading each row
+            if (row_count % print_interval) == 0:
+                now = time.time()
+                print  '{} rows processed ({} lines in {:.2} seconds)'.format(str(row_count), print_interval, now-last)
+                last=now
+
+        print 'Repair of table ' + cass_table + ' {} seconds'.format(time.time() - start_time)
+        print str(row_count) + ' rows read and repaired'
+
+if __name__ == "__main__":
+    main()
